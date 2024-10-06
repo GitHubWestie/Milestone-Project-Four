29f10f568ef424a12fe634fa63095583ffe2f06d
Keiran West <keiran.west@outlook.com>
2024-10-06 17:04:40 +0000

    feat: Added next and previous navigation to lesson template

--- courses/views.py
+++ courses/views.py
@@ -14,4 +14,13 @@ def get_lesson(request, uuid):
     
     lesson = get_object_or_404(Lesson, uuid=uuid)
 
-    return render(request, 'courses/lesson.html', {'lesson':lesson})
+    next_lesson = Lesson.objects.filter(order__gt=lesson.order).order_by('order').first()
+    previous_lesson = Lesson.objects.filter(order__lt=lesson.order).order_by('-order').first()
+
+    context = {
+        'lesson':lesson,
+        'next_lesson':next_lesson,
+        'previous_lesson':previous_lesson,
+    }
+
+    return render(request, 'courses/lesson.html', context)
