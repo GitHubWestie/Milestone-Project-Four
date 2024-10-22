f918a43027ccbbd6b0b1d661ee971c71812ef1b2
Keiran West <keiran.west@outlook.com>
2024-10-22 17:37:22 +0000

    feat: Added meta data to template head
    Removed redundant styles from .hero class

--- CodeFusion/settings.py
+++ CodeFusion/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-githubwesti-milestonepr-hjibg5z0llt.ws.codeinstitute-ide.net',
